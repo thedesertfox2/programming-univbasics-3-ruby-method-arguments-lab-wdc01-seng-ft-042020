cafcf5dbb8ab88a4b1728b5bafe79c21c6c5acd8
carefree-engine-5533 <alexander.gautherin@uconn.edu>
2020-04-08 16:22:22 +0000

    Done.

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -3,7 +3,44 @@
 # thing is NOT to "complete labs," the important thing is to learn the skills you need
 # in order to complete the rest of this course and be ready for the next course!
 
+name = "Josh"
 
+def introduction (name)
+  puts "Hi, my name is #{name}.\n"
+end
+
+introduction name
+#######################################################
+here = "Josh"
+
+def introduction_with_language name, language = "Ruby"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
+end
+
+introduction_with_language here
+
+##########################################################
+
+name_1 = "Edwin"
+
+
+def introduction_with_language_optional name, language = "Ruby"
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
+end
+
+introduction_with_language_optional name_1
+
+#############################################################
+
+name_2 = "Edwin"
+language_1 = "Ruby"
+
+
+def introduction_with_language_optional_1 name, language
+  puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
+end
+
+introduction_with_language_optional_1 name_2, language_1
 
 
 
