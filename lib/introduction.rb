1d809ea3d085c8fcf40f1b3526326f1b5181dfc6
carefree-engine-5533 <alexander.gautherin@uconn.edu>
2020-04-08 16:15:01 +0000

    Automatically backed up by Learn

--- lib/introduction.rb
+++ lib/introduction.rb
@@ -17,7 +17,7 @@ def introduction_with_language name, language = "Ruby"
   puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
 end
 
-introduction_with_language here
+introduction_with_language ("Josh", "Ruby")
 
 ##########################################################
 name = "Josh"
@@ -27,7 +27,7 @@ def introduction_with_language_optional (name, language)
   puts "Hi, my name is #{name} and I am learning to program in #{language}.\n"
 end
 
-introduction_with_language_optional 
+introduction_with_language_optional ""
 
 
 
